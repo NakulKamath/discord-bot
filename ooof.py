016f1317e1f5d13999e9e972ea4ea7377121c160
CrafterMatrix <69424522+CrafterMatrix@users.noreply.github.com>
2021-03-12 11:04:56 +0530

    ye

--- ooof.py
+++ ooof.py
@@ -31,7 +31,8 @@ kicks = False
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 @bot.command(aliases=["to"])
 @commands.has_permissions(manage_messages=True)
-async def ticketoption(context, param=None, emoji=None, *, op=None):
+async def ticketoption(context, param=None, emoji: discord.Emoji=None, *, op=None):
+    print(emoji)
     if param.upper() == 'ADD':
         if emoji == None:
             em = discord.Embed(description=f"{bot.CROSS_MARK} You must provide an emoji/option!\n Usage- `$ticketoption add (emoji) (option line)`")
@@ -1029,47 +1030,45 @@ async def on_raw_reaction_add(payload):
     em.set_author(name=member.name + "#" + member.discriminator, icon_url=member.avatar_url)
     em.set_footer(text="MESSAGE ID: " + str(message.id))
     await log_chat.send(embed=em)
-    topic = ''
     if message.id == bot.data['ticket']['msg'][str(guild.id)]:
-        mesbool = True
-        emojis = ''
-        options = ""
-        for emoji, op in bot.data['ticket']['op'][str(guild.id)].items():
-            options += str(emoji) + " - " + op + "\n"
-        if options != '':
-            em = discord.Embed(title='Select the type of issue!', description=f"{options}")
-            mes = await channel.send(embed=em)
-            for emoji in bot.data['ticket']['op'][str(guild.id)].values():
-                await mes.add_reaction(str(emoji))
-                emojis += str(emoji) + ', ' 
-            await mes.add_reaction(bot.CROSS_MARK)
-            emojis += bot.CROSS_MARK
-            def check(reaction, user):
-                return reaction.message.id == mes.id and str(emoji) in [emojis] and user.id == member.id
-            try:
-                reaction, user = await bot.wait_for('reaction_add', check=check, timeout=30)
-            except asyncio.TimeoutError:
-                em = discord.Embed(description=f"{bot.CROSS_MARK}  You took too long to respond! Cancelling process!")
-                mem = await channel.send(embed=em)
-                await asyncio.sleep(5)
-                await mem.delete()
-                return
-            user = user
-            if str(reaction.emoji) in bot.data['ticket']['op'][str(guild.id)]:
-                topic = bot.data['ticket']['op'][str(guild.id)][str(reaction.emoji)]
+        em = discord.Embed(description=f"{member.mention} Are you sure?")
+        mes = await channel.send(embed=em)
+        await mes.add_reaction(bot.TICK_MARK)
+        await mes.add_reaction(bot.CROSS_MARK)
+        def check(reaction, user):
+            return reaction.message.id == mes.id and str(reaction.emoji) in [bot.TICK_MARK, bot.CROSS_MARK] and user.id == member.id
+        try:
+            r, u = await bot.wait_for('reaction_add', timeout= 30, check=check)
+        except asyncio.TimeoutError:
+            em = discord.Embed(description=f"{bot.CROSS_MARK} You ran out of time! Please re-type the command!")
+            me = await channel.channel.send(embed=em)
+            await asyncio.sleep(5)
+            await mes.delete()
+            await me.delete()
+            return
+        if str(r.emoji) == bot.CROSS_MARK:
+            em = discord.Embed(description=f"{bot.CROSS_MARK} {member.mention} Cancelling process!")
+            memm = await channel.send(embed=em)
+            await asyncio.sleep(5)
+            await mes.delete()
+            await memm.delete()
+            await message.remove_reaction(emoji, member)
+            return
+        if str(r.emoji) == bot.TICK_MARK:
+            em = discord.Embed(description=f"{bot.TICK_MARK} {member.mention} Creating ticket!")
+            memm = await channel.send(embed=em)
         overwrites = {
         guild.default_role: discord.PermissionOverwrite(read_messages=False),
         member: discord.PermissionOverwrite(read_messages=True)}
         chn = await guild.create_text_channel(f"#{bot.data['ticket']['count'][str(guild.id)]}-{member.name}", overwrites=overwrites)
         em = discord.Embed(title=f"Ticket #{bot.data['ticket']['count'][str(guild.id)]}")
         em.add_field(name='Creator', value=member.mention)
-        if topic != '':
-            em.add_field(name='Topic', value=topic)
         await chn.send(embed=em)
         await message.remove_reaction(emoji, member)
         bot.data['ticket']['count'][str(guild.id)] = bot.data['ticket']['count'][str(guild.id)] + 1
-        if mesbool == True:
-            await mes.delete()
+        await asyncio.sleep(5)
+        await mes.delete()
+        await memm.delete()
 
     await save()
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
